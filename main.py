932ff760ef54a1603b58a4cc038fef4d1b145d3d
Clara Galimberti <clara.galimberti@supsi.ch>
2025-03-07 14:08:32 +0100

    Add training of PINN

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ def grad(outputs, inputs):
 
 def physics_loss(model: torch.nn.Module):
     """The physics loss of the model"""
-    # make collocation points
+    r = 0.005
+    temp_env = .25
+    # create points in time (to evaluate the model ODE)
     ts = torch.linspace(0, 1000, steps=1000,).view(-1,1).requires_grad_(True)
-    # run the collocation points through the network
+    # get points in temperature (to evaluate the model ODE)
     temps = model(ts)
     # get the gradient
     dT = grad(temps, ts)[0]
     # compute the ODE
-    ode = dT - model.r * (model.Tenv - temps)
+    ode = dT - r * (temp_env - temps)
     # MSE of ODE
     return torch.mean(ode**2)
 
@@ -29,12 +31,19 @@ def physics_loss(model: torch.nn.Module):
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.hidden_layer = torch.nn.Linear(2,5)
+        self.hidden_layer_1 = torch.nn.Linear(1,5)
+        self.hidden_layer_2 = torch.nn.Linear(5, 20)
+        self.hidden_layer_3 = torch.nn.Linear(20, 5)
         self.output_layer = torch.nn.Linear(5,1)
 
     def forward(self, t):
-        layer_out = torch.sigmoid(self.hidden_layer(t))
-        output = self.output_layer(layer_out)
+        layer_hidden_1 = self.hidden_layer_1(t)
+        sigmoid_layer_hidden_1 = torch.relu(layer_hidden_1)
+        layer_hidden_2 = self.hidden_layer_2(sigmoid_layer_hidden_1)
+        sigmoid_layer_hidden_2 = torch.relu(layer_hidden_2)
+        layer_hidden_3 = self.hidden_layer_3(sigmoid_layer_hidden_2)
+        sigmoid_layer_hidden_3 = torch.relu(layer_hidden_3)
+        output = self.output_layer(sigmoid_layer_hidden_3)
         return output
 
 
@@ -42,7 +51,7 @@ class SystemTemperature(torch.nn.Module):
     def __init__(self, temp_init):
         super().__init__()
         self.r = 0.005
-        self.temp_env = 25
+        self.temp_env = .25
         self.temp_init = temp_init
 
     def forward(self, ts):
@@ -50,11 +59,38 @@ class SystemTemperature(torch.nn.Module):
         return temp_current
 
 
-# Generate the dataset:
-true_model = SystemTemperature(temp_init=100)
-t_data = torch.linspace(0, 300, 10)
-temp_data = true_model(t_data)
-plt.scatter(t_data, temp_data)
-plt.show()
+# Hyperparameters:
+alpha = 1000
 
-#
+# Generate the dataset:
+true_model = SystemTemperature(temp_init=1)
+t_data = torch.linspace(0, 300, 10).unsqueeze(1)
+temp_data = true_model(t_data)
+
+# Model to train:
+model = Net()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+loss_MSE = torch.nn.MSELoss()
+
+
+for epoch in range(int(1e5)):
+    optimizer.zero_grad()
+    temp = model(t_data)
+    loss_1 = loss_MSE(temp, temp_data)
+    loss_2 = physics_loss(model)
+    loss = loss_1 + alpha * loss_2
+    if epoch % 1000 == 0:
+        print("Epoch: %i ---||--- Loss MSE: %.4f --- Loss Physics: %.4f" % (epoch, 1e6*loss_1, 1e6*loss_2))
+    loss.backward()
+    optimizer.step()
+
+# Plot after training:
+t_data_ext = torch.linspace(0, 1000, 50).unsqueeze(1)
+temp_data_ext = true_model(t_data_ext)
+temp_ext = model(t_data_ext)
+
+plt.scatter(t_data, temp_data, label="Data")
+plt.plot(t_data_ext, temp_data_ext, label="True model")
+plt.plot(t_data_ext, temp_ext.detach(), label="Trained model")
+plt.legend()
+plt.show()
