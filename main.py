c37b3816aeaaea8395a14e51f310ca6c9fead1da
Clara Galimberti <clara.galimberti@supsi.ch>
2025-03-12 13:59:02 +0100

    update params values

--- main.py
+++ main.py
@@ -12,10 +12,8 @@ def grad(outputs, inputs):
         create_graph=True
     )
 
-def physics_loss(model: torch.nn.Module):
+def physics_loss(model: torch.nn.Module, r, x_bar):
     """The physics loss of the model"""
-    r = 0.005
-    x_bar = .25
     # create points in time (to evaluate the model ODE)
     ts = torch.linspace(0, 1000, steps=1000,).view(-1,1).requires_grad_(True)
     # get points in x (to evaluate the model ODE)
@@ -32,17 +30,17 @@ class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.hidden_layer_1 = torch.nn.Linear(1,5)
-        self.hidden_layer_2 = torch.nn.Linear(5, 20)
-        self.hidden_layer_3 = torch.nn.Linear(20, 5)
+        self.hidden_layer_2 = torch.nn.Linear(5, 5)
+        self.hidden_layer_3 = torch.nn.Linear(5, 5)
         self.output_layer = torch.nn.Linear(5,1)
 
     def forward(self, t):
         layer_hidden_1 = self.hidden_layer_1(t)
-        sigmoid_layer_hidden_1 = torch.relu(layer_hidden_1)
+        sigmoid_layer_hidden_1 = torch.tanh(layer_hidden_1)
         layer_hidden_2 = self.hidden_layer_2(sigmoid_layer_hidden_1)
-        sigmoid_layer_hidden_2 = torch.relu(layer_hidden_2)
+        sigmoid_layer_hidden_2 = torch.tanh(layer_hidden_2)
         layer_hidden_3 = self.hidden_layer_3(sigmoid_layer_hidden_2)
-        sigmoid_layer_hidden_3 = torch.relu(layer_hidden_3)
+        sigmoid_layer_hidden_3 = torch.tanh(layer_hidden_3)
         output = self.output_layer(sigmoid_layer_hidden_3)
         return output
 
@@ -51,16 +49,16 @@ class System(torch.nn.Module):
     def __init__(self, x_init):
         super().__init__()
         self.r = 0.005
-        self.x_bar = .25
+        self.x_bar = .5
         self.x_init = x_init
 
     def forward(self, ts):
-        x_current = (self.x_init - self.x_bar) * torch.exp(-self.r * ts) + self.x_bar
+        x_current = -(self.x_init - self.x_bar) * torch.exp(-self.r * ts) + self.x_bar
         return x_current
 
 
 # Hyperparameters:
-alpha = 1000
+alpha = 100
 
 # Generate the dataset:
 true_model = System(x_init=1)
@@ -73,11 +71,11 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 loss_MSE = torch.nn.MSELoss()
 
 
-for epoch in range(int(1e5)):
+for epoch in range(int(2e4)):
     optimizer.zero_grad()
     x = model(t_data)
     loss_1 = loss_MSE(x, x_data)
-    loss_2 = physics_loss(model)
+    loss_2 = physics_loss(model, true_model.r, true_model.x_bar)
     loss = loss_1 + alpha * loss_2
     if epoch % 1000 == 0:
         print("Epoch: %i ---||--- Loss MSE: %.4f --- Loss Physics: %.4f" % (epoch, 1e6*loss_1, 1e6*loss_2))
@@ -88,9 +86,15 @@ for epoch in range(int(1e5)):
 t_data_ext = torch.linspace(0, 1000, 50).unsqueeze(1)
 x_data_ext = true_model(t_data_ext)
 x_ext = model(t_data_ext)
+# Loss after training:
+loss_1 = loss_MSE(x, x_data)
+loss_2 = physics_loss(model, true_model.r, true_model.x_bar)
+loss = loss_1 + alpha * loss_2
+print("Epoch: %i ---||--- Loss MSE: %.4f --- Loss Physics: %.4f" % (epoch, 1e6*loss_1, 1e6*loss_2))
 
 plt.scatter(t_data, x_data, label="Data")
 plt.plot(t_data_ext, x_data_ext, label="True model")
 plt.plot(t_data_ext, x_ext.detach(), label="Trained model")
 plt.legend()
+plt.savefig("plot.pdf", format="pdf")
 plt.show()
